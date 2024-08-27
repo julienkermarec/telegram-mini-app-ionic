fd5ecb3fd2ec4e5249bf9d4e96ef54c6d2b4a94d
Julien Kermarec <julien.kermarec@gmail.com>
2024-08-27 06:15:35 +0100

    Auto-generated commit

---
+++ 4292.fd9ea38e54b68157.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4292],{4292:(u,n,e)=>{e.r(n),e.d(n,{SplashScreenWeb:()=>_});var s=e(467),r=e(5083);class _ extends r.E_{show(a){return(0,s.A)(function*(){})()}hide(a){return(0,s.A)(function*(){})()}}}}]);
